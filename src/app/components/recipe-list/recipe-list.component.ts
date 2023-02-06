d8f7a47d25f0c2e81e2527d3179df50b26aed0cc
sergiowillpugliese <pugliese.sergio1987@hotmail.it>
2023-02-06 15:16:39 +0100

    componenti ricette

---
+++ src/app/components/recipe-list/recipe-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+recipes: Recipe[] = [
+  new Recipe('Zuppa di qualcosa', 'Sembra buona. Ho fame!', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwdnmHWH5UaAfu-7TJJr0A_3WIP3tykBJkgw&usqp=CAU'),
+  new Recipe('Zuppa di qualcosa', 'Sembra buona. Ho fame!', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwdnmHWH5UaAfu-7TJJr0A_3WIP3tykBJkgw&usqp=CAU'),
+  new Recipe('Zuppa di qualcosa', 'Sembra buona. Ho fame!', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwdnmHWH5UaAfu-7TJJr0A_3WIP3tykBJkgw&usqp=CAU')
+
+];
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+      
+  }
+
+}
